a0df2d8028385df71aed34d4722feee44bdeb551
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-12-29 16:38:41 +0700

    membuat appplication untuk app yang akan di jalankan nantinya

---
+++ app/src/main/java/com/tugas/dataprojek2/MahasiswaApplications.kt
@@ -0,0 +1,2 @@
+package com.tugas.dataprojek2
+
---
+++ app/src/main/java/com/tugas/dataprojek2/container/MahasiswaContainer.kt
@@ -0,0 +1,28 @@
+package com.tugas.dataprojek2.container
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.tugas.dataprojek2.Repositori.MahasiswaRepository
+import com.tugas.dataprojek2.Repositori.NetworkKontakRepository
+import com.tugas.dataprojek2.service.MahasiswaService
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer{
+    val kontakRepository: MahasiswaRepository
+}
+
+class MahasiswaContainer : AppContainer{
+    private val baseUrl ="http://10.0.2.2:8000/umyTI/" //localhost diganti ip kalau run di hp
+    private val json = Json{ignoreUnknownKeys = true}
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val mahasiswaService: MahasiswaService by lazy {
+        retrofit.create(MahasiswaService::class.java) }
+
+    override val kontakRepository: MahasiswaRepository by lazy{
+        NetworkKontakRepository(mahasiswaService)
+    }
+}

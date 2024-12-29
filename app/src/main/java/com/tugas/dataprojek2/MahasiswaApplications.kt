efaf7256d9ecf4006635efab59fa50a0c821a1b5
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-12-29 16:51:25 +0700

    menambah MahasiswaApplications

--- app/src/main/java/com/tugas/dataprojek2/MahasiswaApplications.kt
+++ app/src/main/java/com/tugas/dataprojek2/MahasiswaApplications.kt
@@ -1,2 +1,13 @@
 package com.tugas.dataprojek2
 
+import android.app.Application
+import com.tugas.dataprojek2.container.AppContainer
+import com.tugas.dataprojek2.container.MahasiswaContainer
+
+class MahasiswaApplications: Application() {
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container= MahasiswaContainer()
+    }
+}

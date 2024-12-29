95e9d52d22d2a92a4749bb497a163d9eeb1c4aa2
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-12-29 16:36:01 +0700

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,85 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
+}
+
+android {
+    namespace = "com.tugas.dataprojek2"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.tugas.dataprojek2"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+
+    dependencies {
+
+        // Import the Compose BOM
+        implementation(libs.androidx.core.ktx)
+        implementation(libs.androidx.lifecycle.runtime.ktx)
+        implementation(libs.androidx.activity.compose)
+        implementation(platform(libs.androidx.compose.bom))
+        implementation(libs.androidx.ui)
+        implementation(libs.androidx.ui.graphics)
+        implementation(libs.androidx.ui.tooling.preview)
+        implementation(libs.androidx.material3)
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.androidx.junit)
+        androidTestImplementation(libs.androidx.espresso.core)
+        androidTestImplementation(platform(libs.androidx.compose.bom))
+        androidTestImplementation(libs.androidx.ui.test.junit4)
+        debugImplementation(libs.androidx.ui.tooling)
+        debugImplementation(libs.androidx.ui.test.manifest)
+
+// Retrofit
+        implementation(libs.retrofit2.kotlinx.serialization.converter)
+        implementation(libs.retrofit)
+        implementation(libs.okhttp)
+        implementation(libs.coil.compose)
+        implementation(libs.kotlinx.serialization.json)
+
+
+
+        implementation(libs.androidx.lifecycle.viewmodel.compose)
+        implementation(libs.androidx.navigation.compose)
+    }
+}
